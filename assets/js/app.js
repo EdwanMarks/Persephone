ff36fc4458bc7217587115706270aea25998a7ce
EdwanMarks <99619218+EdwanMarks@users.noreply.github.com>
2023-11-19 07:41:03 -0300

    Atualizar o app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,11 @@ document.addEventListener("click", function() {
     var audio = document.querySelector("audio");
     audio.play();
   });
+
+// Adiciona outro evento ao documento
+document.addEventListener("beforeunload", function() {
+    var audio = document.querySelector("audio");
+    audio.pause();
+  });
+
   
